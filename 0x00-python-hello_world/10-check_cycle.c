bb9bad1fbd7afa309e7b1482ffa70aa51fa9d1c1
Lohkrii <joshualopez0429@gmail.com>
2020-12-08 01:57:19 +0000

    Task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Identifies if there is a cycle in a singly linked list.
+ * @list: Is the list in question.
+ * Return: Returns 1 for Yes in a cycle. 0 if not.
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t* curr_node = list, nav_tool = curr_node;
+
+	for (;curr_node && nav_tool && curr_node ->next;)
+	{
+		nav_tool = nav_tool->next;
+		curr_node = curr_node->next->next;
+		if (curr_node == nav_tool)
+			return (1);
+	}
+	return (0);
+}
