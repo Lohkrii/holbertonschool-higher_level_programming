e22929f655f7dbdb7675ff86adf544c392512d6d
Lohkrii <joshualopez0429@gmail.com>
2020-12-09 17:33:51 +0000

    task 1 partial

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number > 5:
+    last_num = number % 10
+    spec_str = "greater than 5"
+elif number < 0
+print("Last digit of {:d} is {:d} and is {:s}".format(number, )
