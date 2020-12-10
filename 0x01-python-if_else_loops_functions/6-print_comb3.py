34521cb59f4180e1dfe37fba0d24269b31b90590
Lohkrii <joshualopez0429@gmail.com>
2020-12-10 03:36:22 +0000

    Task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for idx in range(0, 10):
+    for cidx in range(idx + 1, 10):
+        print ("{:d}{:d}".format(idx, cidx), end ="")
+        if idx * 10 + cidx != 89:
+            print(", ", end="")
+print("")
