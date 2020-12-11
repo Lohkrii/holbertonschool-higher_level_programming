d4c23f2975dc93c04ab4eeb57f12cecde3345d36
Lohkrii <joshualopez0429@gmail.com>
2020-12-11 01:06:27 +0000

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    if len(argv) == 2:
+        count_arg = "argument:"
+    elif len(argv) < 2:
+        count_arg = "arguments."
+    else:
+        count_arg = "arguments:"
+    print("{} {}".format(len(argv) - 1, count_arg))
+    for idx in range(1, len(argv)):
+        print ("{}: {}".format(idx, argv[idx]))
