39f5162c638c0f435a4a29f78b18efa6a7a69a7a
Lohkrii <joshualopez0429@gmail.com>
2020-12-16 01:24:43 +0000

    Update tsk 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new = ""
     for idx in my_string:
-        if my_string[idx] is not "c" and idx is not "C":
+        if my_string[idx] is not "c" and my_string[idx] is not "C":
             new += idx
     return(new)
