af2a7d4011ed8e034d2351e750d473829401f412
Lohkrii <joshualopez0429@gmail.com>
2020-12-18 03:49:52 +0000

    Fixed my dyslexic ass

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(my_dictionary):
-    new_dict = dict(my_dictionary)
+    new_dictionary = dict(my_dictionary)
     for key, value in my_dictionary.items():
         new_dictionary[key] = value * 2
     return new_dictrionary
