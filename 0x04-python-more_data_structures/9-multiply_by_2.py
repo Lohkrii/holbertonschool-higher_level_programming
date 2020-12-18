24056ad7d090d350af9b4c3970d1fcf47eb37beb
Lohkrii <joshualopez0429@gmail.com>
2020-12-18 03:51:02 +0000

    Fixed my dyslxic arms too

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,4 +3,4 @@ def multiply_by_2(my_dictionary):
     new_dictionary = dict(my_dictionary)
     for key, value in my_dictionary.items():
         new_dictionary[key] = value * 2
-    return new_dictrionary
+    return new_dictionary
