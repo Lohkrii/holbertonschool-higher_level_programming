dd6f3d58c6311a2285a2b7d2c62322a6995779d9
Lohkrii <joshualopez0429@gmail.com>
2021-01-21 22:34:37 +0000

    Update to tsk 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Verifies if object is inherited of a specified class. """
 
+
 def inherits_from(obj, a_class):
     """ Check if class of obj is of specified class """
     if type(obj) is not a_class and issubclass(type(obj), a_class):
