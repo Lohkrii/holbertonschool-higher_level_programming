e221ec51b61a4adbd95c02f432452a45fa51a08d
Lohkrii <joshualopez0429@gmail.com>
2021-01-21 22:39:02 +0000

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Exception raiser of inherited class """
+
+
+def area(self):
+    """BaseGeometry"""
+    def area(self):
+        """ Exception raiser """
+        raise Exception("area() is not implemented")
