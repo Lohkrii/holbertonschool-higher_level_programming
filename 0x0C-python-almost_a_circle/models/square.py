eaaee22ef2954310dab5a6768362e6cca0635b1c
Lohkrii <joshualopez0429@gmail.com>
2021-01-30 02:11:18 +0000

    Task 10: Square init

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,11 +2,11 @@
 """
 Square Class creation
 """
-from models import Base
-from models import Rectangle
+from models.base import Base
+from models.rectangle import Rectangle
 
 
-class Square():
+class Square(Rectangle):
     """Class initialization"""
     def __init__(self, size, x=0, y=0, id=None):
         """Initializing Values"""
@@ -14,13 +14,15 @@ class Square():
 
     @property
     def size(self):
+        """size getter"""
         return self.width
 
     @size.setter
     def size(self, my_size):
-        self.height and self.width = my_size
+        """size setter"""
+        self.height = my_size
+        self.width = my_size
 
     def __str__(self):
         return "[Square] ({}) {}/{} - {}"\
             .format(self.id, self.x, self.y, self.width)
-
