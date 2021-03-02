70a65e3681ac5c896e5b08fd75806e216343c571
Lohkrii <joshualopez0429@gmail.com>
2021-03-02 18:35:49 +0000

    1-create_user.sql: Rm \n

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- Creates a user in localhost server username: user_0d_1.
-
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
