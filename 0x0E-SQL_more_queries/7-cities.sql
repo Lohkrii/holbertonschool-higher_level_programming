311bb7f7c93d65b3d4c1f2a59bca10836cf8488a
Lohkrii <joshualopez0429@gmail.com>
2021-03-02 19:17:33 +0000

    7-cities.sql: Creates a new database and cities table referencing states table for the foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- Creates the database hbtn_0c_usa and cities table.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+	id INT UNIQUE NOT NULL AUTO_INCREMENT,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY(states_id) REFERENCES hbtn_0d_usa.states(id)
+);
