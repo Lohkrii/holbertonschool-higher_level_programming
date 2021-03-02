76a6ace0834086c087f39f5519241d988105ee66
Lohkrii <joshualopez0429@gmail.com>
2021-03-02 01:23:24 +0000

    100-move_to_utf8.sql: Restructured some of the code

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,5 @@
 -- Converts hbtn_0c_0, first_table, field `name` in first_table to utf-8.
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 USE hbtn_0c_0;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
