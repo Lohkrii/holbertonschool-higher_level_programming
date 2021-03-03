0050f0bb6bb6ddfbeeb6c37ec2859679c25d7e51
Lohkrii <joshualopez0429@gmail.com>
2021-03-03 01:54:18 +0000

    Restructured code: was doing it backwards

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,9 +1,10 @@
 -- Lists all genres from hbtn_0d_tvshows and displays the number of
 -- shows linked to each one.
 SELECT tv_genres.name
-AS genre, COUNT(*)
-FROM tv_show_genres
-INNER JOIN tv_genres
-ON tv_show_genres.genre_id=tv_genres.id
-GROUP BY tv_genres.name
+AS genre, COUNT(tv_show_genres.genre_id)
+AS show_qty
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id=tv_show_genres.genre_id
+GROUP BY genre
 ORDER BY number_shows DESC;
