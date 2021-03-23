d3d64063c0b6ae7c9dee68628796999c39cf39ce
Lohkrii <joshualopez0429@gmail.com>
2021-03-23 18:41:30 +0000

    Forgot to rename varibale

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/nodejs
 // Prints 3 lines of string.
-const fVar = 'C is fun\nPython is cool\nJavascript is amazing';
+const mySTR = 'C is fun\nPython is cool\nJavascript is amazing';
 console.log(mySTR);
