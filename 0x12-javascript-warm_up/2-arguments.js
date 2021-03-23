24ffa66cead655d1168556bd6369c88b08d28d87
Lohkrii <joshualopez0429@gmail.com>
2021-03-23 21:51:04 +0000

    Fixed came case and other semistandard compliance fixes

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/nodejs
-const arg_count = process.argv.length;
-if (arg_count <= 2) {
+const argCount = process.argv.length;
+if (argCount <= 2) {
   console.log('No argument');
-} else if (arg_count == 3) {
+} else if (argCount === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
