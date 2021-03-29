4476ade922fe1df35cf18fd31a20bf9de61cb452
Lohkrii <joshualopez0429@gmail.com>
2021-03-29 02:18:53 +0000

    Removed code to comply with exceptions

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -13,8 +13,7 @@ if __name__ == '__main__':
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states\
-                WHERE BINARY name = %s\
-                ORDER BY id ASC".format(sys.argv[4]))
+                WHERE name = %s".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
