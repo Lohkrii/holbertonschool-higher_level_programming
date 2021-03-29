f052f6d737fb21ea9ad3610ca15f37c6b2a31ada
Lohkrii <joshualopez0429@gmail.com>
2021-03-29 01:04:08 +0000

    More typos

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,19 +2,19 @@
 
 
 def connectToDB():
-    db = MySQLdb.connect(host=localhost,
-                         user=sys.argv(1),
-                         passwd=sys.argv(2),
-                         port=3306,
-                         db=sys.argv(3))
-
+    db = MySQLdb.connect(host='localhost', port=3306,
+                           user=sys.argv[1], passwd=sys.argv[2],
+                           db=sys.argv[3], charset='utf8')
     cur = db.cursor()
+
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
+
     cur.close()
-    conn.close()
+    db.close()
+
 if __name__ == '__main__':
     import MySQLdb
     import sys
